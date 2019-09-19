76a23b77a7abc10512ba5df29072e187ec91c5a8
김지윤 <kimjiyoon1006@hanmail.net>
2019-09-19 21:23:36 +0900

    first second

--- first.js
+++ first.js
@@ -1,16 +1,30 @@
-// Run by Node.js
-// 369 게임
-// node first.js 해보면 input output이 어떻게 되는지 알 수 있음
+const readline = require('readline');
 
-const readline = require("readline");
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
+const rl = readline.createInterface(
+    {input: process.stdin, output: process.stdout}
+);
 
-rl.on("line", function(line) {
-	console.log("Hello Goorm! Your input is", line);
-	rl.close();
-}).on("close", function() {
-	process.exit();
+rl.on('line', function (answer) {
+    var num = '';
+    var count = 0;
+
+    for (var i = 1; i <= answer; i++) {
+
+        num += i;
+
+    }
+    num = num.split('');
+
+    for (var i = 0; i < num.length; i++) {
+        if (num[i] == 3) {
+            count++;
+        } else if (num[i] == 6) {
+            count++;
+        } else if (num[i] == 9) {
+            count++;
+        }
+    }
+    console.log(count);
+
+    rl.close();
 });
--- second.js
+++ second.js
@@ -1,16 +1,14 @@
-// Run by Node.js
-// 은행 예금 이자 구하기
-// node second.js 해보면 input output이 어떻게 되는지 알 수 있음
+const readline = require('readline');
 
-const readline = require("readline");
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
+const rl = readline.createInterface(
+    {input: process.stdin, output: process.stdout}
+);
 
-rl.on("line", function(line) {
-	console.log(line);
-	rl.close();
-}).on("close", function() {
-	process.exit();
-});
+rl.on('line', function (answer) {
+    answer = answer.split(' ');
+    pr = parseInt(answer[0]);
+    for (var i = 0; i < answer[2]; i++) {
+        pr += pr * parseFloat(answer[1] * 0.01)
+    }
+
+    console.log(pr.toFixed(2));
